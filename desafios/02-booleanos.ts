a316ec32d60569d1e42b00f28d17a7bfc4785ec7
Karoline <kkohlsstangherlin@gmail.com>
2023-06-26 16:35:57 -0300

    :recycle: [refactor]: Código limpo em Booleanos

--- desafios/02-booleanos.ts
+++ desafios/02-booleanos.ts
@@ -1,8 +1,8 @@
 // Boleanos
 
 const user = {
-  name: 'Diego Fernandes',
-  height: 190,
+  name: 'Karol',
+  height: 166,
   hasTicket: true,
 }
 
@@ -10,22 +10,22 @@ const necessaryHeight = 130
 
 const currentHour = new Date().getHours()
 
-const parkOpen = currentHour > 9 && currentHour < 18
+const isParkOpen = currentHour > 9 && currentHour < 18
 
-if (!parkOpen) {
+if (!isParkOpen) {
   throw new Error('O parque está fechado!')
 }
 
-const ticket = user.hasTicket
+const hasTicket = user.hasTicket
 
-if (!ticket) {
-  throw new Error('O Diego não possui um bilhete para entrar no parque!')
+if (!hasTicket) {
+  throw new Error('A Karol não possui um bilhete para entrar no parque!')
 }
 
-const enterToy = user.height > necessaryHeight
+const doesEnterToy = user.height > necessaryHeight
 
-if (!enterToy) {
-  throw new Error('O Diego não pode entrar no brinquedo!')
+if (!doesEnterToy) {
+  throw new Error('A Karol não pode entrar no brinquedo!')
 } 
 
-console.log('O Diego se divertiu muito! :)')
+console.log('A Karol se divertiu muito! :)')
